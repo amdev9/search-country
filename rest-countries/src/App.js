9f213f916d6339488fd6524f98dbfe668bd7ac53
Alex Matveev <amatveevdev9@gmail.com>
2022-01-11 20:33:16 +0300

    responsive country

--- rest-countries/src/App.js
+++ rest-countries/src/App.js
@@ -3,18 +3,14 @@ import Countries from "./components/Countries";
 import Country from "./components/Country";
 import { StateProvider } from "./state/store.js";
 
-import "./App.scss";
-
 function App() {
   return (
     <StateProvider>
       <Router>
-        <div className="App">
-          <Switch>
-            <Route exact path={["/", "/countries"]} component={Countries} />
-            <Route path="/countries/:id" component={Country} />
-          </Switch>
-        </div>
+        <Switch>
+          <Route exact path={["/", "/countries"]} component={Countries} />
+          <Route path="/countries/:id" component={Country} />
+        </Switch>
       </Router>
     </StateProvider>
   );
--- rest-countries/src/components/Country.jsx
+++ rest-countries/src/components/Country.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { store } from "../state/store.js";
 
-import Button from './UI/Button';
+import Button from "./UI/Button";
 import BorderCountries from "./BorderCountries";
 import Layout from "./Layout";
 import styles from "./Country.module.scss";
@@ -19,51 +19,53 @@ const Country = () => {
 
   return (
     <Layout>
-      <div className={styles.back}>
-        <Button onClick={() => history.goBack()}>Back</Button>
-      </div>
-      <div className={styles.wrapper}>
-        <img className={styles.img} src={item.flags.png} alt={item.name} />
+      <div className={styles.container}>
+        <div className={styles.back}>
+          <Button onClick={() => history.goBack()}>Back</Button>
+        </div>
+        <div className={styles.wrapper}>
+          <img className={styles.img} src={item.flags.png} alt={item.name} />
 
-        <div className={styles.info}>
-          <span className={styles.name}>{item.name}</span>
-          {/* TODO: use keys transformation as labels */}
-          <div className={styles.info__basic}>
-            <span>
-              <b>Native Name: </b>
-              {item.nativeName}
-            </span>
-            <span>
-              <b>Population: </b> {item.population}
-            </span>
-            <span>
-              <b>Region: </b>
-              {item.region}
-            </span>
-            <span>
-              <b>Sub Region: </b>
-              {item.subregion}
-            </span>
-            <span>
-              <b>Capital: </b>
-              {item.capital}
-            </span>
+          <div className={styles.info}>
+            <span className={styles.name}>{item.name}</span>
+            {/* TODO: use keys transformation as labels */}
+            <div className={styles.info__basic}>
+              <span>
+                <b>Native Name: </b>
+                {item.nativeName}
+              </span>
+              <span>
+                <b>Population: </b> {item.population}
+              </span>
+              <span>
+                <b>Region: </b>
+                {item.region}
+              </span>
+              <span>
+                <b>Sub Region: </b>
+                {item.subregion}
+              </span>
+              <span>
+                <b>Capital: </b>
+                {item.capital}
+              </span>
+            </div>
+            <div className={styles.info__additional}>
+              <span>
+                <b>Top Level Domain: </b>
+                {item.topLevelDomain}
+              </span>
+              <span>
+                <b>Currencies: </b>
+                {item.currencies.map((curr) => curr.name).join(", ")}
+              </span>
+              <span>
+                <b>Languages: </b>
+                {item.languages.map((lang) => lang.name).join(", ")}
+              </span>
+            </div>
+            <BorderCountries borders={item.borders} />
           </div>
-          <div className={styles.info__additional}>
-            <span>
-              <b>Top Level Domain: </b>
-              {item.topLevelDomain}
-            </span>
-            <span>
-              <b>Currencies: </b>
-              {item.currencies.map((curr) => curr.name).join(", ")}
-            </span>
-            <span>
-              <b>Languages: </b>
-              {item.languages.map((lang) => lang.name).join(", ")}
-            </span>
-          </div>
-          <BorderCountries borders={item.borders} />
         </div>
       </div>
     </Layout>
